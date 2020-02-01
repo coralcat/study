690ae9a3cc2868f32e3fb5be79b12b27e082dce3
coralcat <you@example.com>
2020-02-01 14:50:37 +0900

    test push

--- v1/src/pages/index.js
+++ v1/src/pages/index.js
@@ -10,7 +10,7 @@ const IndexPage = () => (
     <SEO title="Home" />
     <h1>Hi people</h1>
     <p>Welcome to Sori's Portfolio.</p>
-    <p>Now go build something great.</p>
+    <p>test push.</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
       <Image />
     </div>
