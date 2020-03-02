af9f451fb76e1ea09ae2272363644d9b2a184dc5
coralcat <you@example.com>
2020-03-02 14:19:17 +0900

    Counter에서 class 제거

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,19 +1,14 @@
 import React from 'react';
 
-export class Counter extends React.Component {
-
-    handleChange(delta) {
-        console.log("handleChange");
-        this.setState(prevState => ({score: this.state.score + delta}));
-    }
-
-    render() {
-        return (
-            <div className="counter">
-                <button className="counter-action decrement" onClick={() => this.props.changeScore(this.props.id, -1)}> -</button>
-                <span className="counter-score">{this.props.score}</span>
-                <button className="counter-action increment" onClick={() => this.props.changeScore(this.props.id, 1)}> +</button>
-            </div>
-        );
-    };
+export const Counter = (props) => {
+    console.log(props);
+    return (
+        <div className="counter">
+            <button className="counter-action decrement" onClick={() => props.changeScore(props.id, -1)}> -
+            </button>
+            <span className="counter-score">{props.score}</span>
+            <button className="counter-action increment" onClick={() => props.changeScore(props.id, 1)}> +
+            </button>
+        </div>
+    );
 };
