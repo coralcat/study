cb7908d49dcdf0b4ec62c1580715c2e709831d1d
coralcat <sori.coralcat@gmail.com>
2020-08-25 11:33:03 +0900

    netlify starter blog 예제를 참고한 부분 전부 제거 후 Algolia 검색 설치

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,3 +1,5 @@
+require("dotenv").config()
+
 module.exports = {
   siteMetadata: {
     title: `Pandas Eating Lots`,
@@ -5,6 +7,14 @@ module.exports = {
     author: `gatsbyjs`,
   },
   plugins: [
+    {
+      resolve: `gatsby-plugin-algolia`,
+      options: {
+        appId: process.env.GATSBY_ALGOLIA_APP_ID,
+        apiKey: process.env.ALGOLIA_ADMIN_KEY,
+        queries: require("./src/utils/algolia-queries")
+      }
+    },
     {
       resolve: `gatsby-source-filesystem`,
       options: {
