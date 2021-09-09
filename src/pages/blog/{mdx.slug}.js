007faffa65ed0b2aef4c35ea6c2c8f7713bf8c78
Sori Kim <sori.coralcat@gmail.com>
2021-09-09 12:07:09 +0900

    Finished Gatsby Tutorial Part 7

--- src/pages/blog/{mdx.slug}.js
+++ src/pages/blog/{mdx.slug}.js
@@ -2,14 +2,22 @@ import * as React from "react";
 import { graphql } from "gatsby";
 import { MDXRenderer } from "gatsby-plugin-mdx"; // highlight-line
 import Layout from "../../components/layout";
+import { GatsbyImage, getImage } from "gatsby-plugin-image";
 
-const BlogPost = ({ data }) => { // highlight-line
+const BlogPost = ({ data }) => {
+  const image = getImage(data.mdx.frontmatter.hero_image); // highlight-line
+
   return (
     <Layout pageTitle={data.mdx.frontmatter.title}>
       <p>{data.mdx.frontmatter.date}</p>
-      <MDXRenderer>
-        {data.mdx.body}
-      </MDXRenderer>
+      <GatsbyImage image={image} alt={data.mdx.frontmatter.hero_image_alt} />
+      <p>
+        Photo Credit:{" "}
+        <a href={data.mdx.frontmatter.hero_image_credit_link}>
+          {data.mdx.frontmatter.hero_image_credit_text}
+        </a>
+      </p>
+      <MDXRenderer>{data.mdx.body}</MDXRenderer>
     </Layout>
   );
 };
@@ -18,9 +26,18 @@ const BlogPost = ({ data }) => { // highlight-line
 export const query = graphql`
   query ($id: String) {
     mdx(id: { eq: $id }) {
+      body
       frontmatter {
         title
         date(formatString: "MMMM D, YYYY")
+        hero_image_alt
+        hero_image_credit_link
+        hero_image_credit_text
+        hero_image {
+          childImageSharp {
+            gatsbyImageData
+          }
+        }
       }
       body
     }
