448eb5a585e8aa2c64c4cb1bb2fb4274170e2d75
김소리 <ksr@gabiacns.com>
2022-05-13 09:06:40 +0900

    웹팩설정에 환경변수 전달

--- webpack/webpack.config.js
+++ webpack/webpack.config.js
@@ -6,89 +6,94 @@ const yaml = require("yamljs");
 const json5 = require("json5");
 const HtmlWebpackPlugin = require("html-webpack-plugin");
 
-module.exports = {
-  mode: "development",
-  entry: {
-    index: "./src/index.js",
-    another: "./src/another-module.js",
-  },
-  plugins: [
-    new HtmlWebpackPlugin({
-      title: "Caching",
-    }),
-  ],
-  devtool: "inline-source-map",
-  devServer: {
-    static: "./dist",
-  },
-  output: {
-    filename: "[name].[contenthash].js",
-    path: path.resolve(__dirname, "dist"),
-    assetModuleFilename: "assets/[hash]",
-    clean: true,
-    publicPath: "/",
-  },
-  optimization: {
-    runtimeChunk: "single",
-    splitChunks: {
-      cacheGroups: {
-        vendor: {
-          test: /[\\/]node_modules[\\/]/,
-          name: "vendors",
-          chunks: "all",
+module.exports = (env) => {
+  console.log("Goal: ", env.goal);
+  console.log("Production: ", env.production);
+
+  return {
+    mode: "development",
+    entry: {
+      index: "./src/index.js",
+      another: "./src/another-module.js",
+    },
+    plugins: [
+      new HtmlWebpackPlugin({
+        title: "Caching",
+      }),
+    ],
+    devtool: "inline-source-map",
+    devServer: {
+      static: "./dist",
+    },
+    output: {
+      filename: "[name].[contenthash].js",
+      path: path.resolve(__dirname, "dist"),
+      assetModuleFilename: "assets/[hash]",
+      clean: true,
+      publicPath: "/",
+    },
+    optimization: {
+      runtimeChunk: "single",
+      splitChunks: {
+        cacheGroups: {
+          vendor: {
+            test: /[\\/]node_modules[\\/]/,
+            name: "vendors",
+            chunks: "all",
+          },
         },
       },
     },
-  },
-  module: {
-    rules: [
-      {
-        test: /\.css$/i,
-        use: ["style-loader", "css-loader"],
-      },
-      {
-        test: /\.(png|svg|jpg|jpeg|gif)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "assets/images/[hash][ext][query]",
+    module: {
+      rules: [
+        {
+          test: /\.css$/i,
+          use: ["style-loader", "css-loader"],
         },
-      },
-      {
-        test: /\.(woff|woff2|eot|ttf|otf)$/i,
-        type: "asset/resource",
-        generator: {
-          filename: "assets/fonts/[hash][ext][query]",
+        {
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "assets/images/[hash][ext][query]",
+          },
         },
-      },
-      {
-        test: /\.(csv|tsv)$/i,
-        use: ["csv-loader"],
-      },
-      {
-        test: /\.xml$/i,
-        use: ["xml-loader"],
-      },
-      {
-        test: /\.toml$/i,
-        type: "json",
-        parser: {
-          parse: toml.parse,
+        {
+          test: /\.(woff|woff2|eot|ttf|otf)$/i,
+          type: "asset/resource",
+          generator: {
+            filename: "assets/fonts/[hash][ext][query]",
+          },
         },
-      },
-      {
-        test: /\.yaml$/i,
-        type: "json",
-        parser: {
-          parse: yaml.parse,
+        {
+          test: /\.(csv|tsv)$/i,
+          use: ["csv-loader"],
         },
-      },
-      {
-        test: /\.json5$/i,
-        type: "json",
-        parser: {
-          parse: json5.parse,
+        {
+          test: /\.xml$/i,
+          use: ["xml-loader"],
         },
-      },
-    ],
-  },
+        {
+          test: /\.toml$/i,
+          type: "json",
+          parser: {
+            parse: toml.parse,
+          },
+        },
+        {
+          test: /\.yaml$/i,
+          type: "json",
+          parser: {
+            parse: yaml.parse,
+          },
+        },
+        {
+          test: /\.json5$/i,
+          type: "json",
+          parser: {
+            parse: json5.parse,
+          },
+        },
+      ],
+    },
+  }
 };
